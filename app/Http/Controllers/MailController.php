af6e5ebdf0ec26e730cfa00467364ed07c41d77f
Yehor Polishchuk <e-dp@ukr.net>
2021-04-10 13:20:11 +0300

    feedback from mailing added

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\FeedbackFormMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -9,6 +10,15 @@ class MailController extends Controller
 {
     public function handleFeedbackForm(Request $request)
     {
-        // TODO!
+        $data = $request->validate([
+            'name' => 'required',
+            'text' => 'required|max:500',
+            'subject' => 'required|max:250',
+            'email' => 'required|email',
+            'tel' => 'present',
+        ]);
+
+        Mail::to(env('SEND_FEEDBACK_FORM_ADDRESS_TO'))
+            ->send(new FeedbackFormMail($data));
     }
 }
---
+++ app/Mail/FeedbackFormMail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class FeedbackFormMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.feedback')->with([
+            'data' => $this->data
+        ])->subject(env('FEEDBACK_FORM_SUBJECT'));
+    }
+}
