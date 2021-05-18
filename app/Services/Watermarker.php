852b47fd2ef7455eeddf1a572600f53a513956e5
Yehor Polishchuk <e-dp@ukr.net>
2021-05-18 20:29:15 +0300

    waterwark and resize added

--- app/Services/Watermarker.php
+++ app/Services/Watermarker.php
@@ -9,13 +9,32 @@ class Watermarker
     private static function makeImgWatermarked($img_path_from, $img_path_to, $watermark_path)
     {
         $img = Image::make($img_path_from);
+        self::minifyImg($img);
 
-        $watermark_obj = Image::make($watermark_path);
-        $watermark_obj->opacity(70);
-        $img->insert($watermark_obj, 'center');
-        // $img->insert($watermark_path, 'center');
-        $img->encode('png');
+        $watermark_img = Image::make($watermark_path);
+        self::fitWatermarkToImg($img, $watermark_img, 2);
+
+        $watermark_img->opacity(70);
+        $img->insert($watermark_img, 'center');
+        // $img->encode('png');
         $img->save($img_path_to);
+    }
+
+    /**
+     * @param Image main_img Image the watermark to be attached to.
+     * @param Image $watermark_img Waterwark image object
+     */
+    private static function fitWatermarkToImg($main_img, $watermark_img, $factor)
+    {
+        $wtm_height_needed = intval(round($main_img->height() / $factor));
+        $watermark_img->heighten($wtm_height_needed);
+    }
+
+    private static function minifyImg($img, $max_height = 720)
+    {
+        if ($img->height() > $max_height) {
+            $img->heighten($max_height);
+        }
     }
 
     /**
